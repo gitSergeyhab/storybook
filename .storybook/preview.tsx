a3dc06c131cc761665bc9e2992e5a00da393f1b0
Sergey <redmoloch@yandex.ru>
2024-10-11 08:36:15 +0300

    fix(sb-6): Delete useless code

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,17 +1,9 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
-import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
 import { Theme } from "../src/types";
 import { themes } from "../src/components/theme-provider/const";
 import { Button } from "../src/components/button";
 import { StyledStoryLayout } from "./StyledStoryLayout";
-
-export const parameters = {
-  actions: { argTypesRegex: "^on.*" },
-  viewport: {
-    viewports: INITIAL_VIEWPORTS,
-  },
-};
 
 const WithThemeProvider = (Story, context) => {
   const [theme, setTheme] = React.useState<Theme>("light");
